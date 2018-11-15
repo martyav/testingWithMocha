61fbb949d0c202ab889dd2f36f931e5d63a934f8
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-11-15 03:31:22 -0500

    shorter solution

--- testSubjects/interviewArchive.js
+++ testSubjects/interviewArchive.js
@@ -2,19 +2,9 @@ let interviewArchive = {};
 
 interviewArchive.smallestPostiveInteger = function(array) {
   let smallest = 1;
-    
-  if (!array.includes(1)) return smallest; 
-    
-  const sorted = array.sort(function(a,b) { return a - b });
-    
-  for (value of sorted) {
-    if (smallest === value) smallest++;
-    else {
-      if (value < smallest) continue;
-      else break;
-    }
-  }
-    
+
+  while (array.includes(smallest)) smallest++;
+
   return smallest;
 }
 
